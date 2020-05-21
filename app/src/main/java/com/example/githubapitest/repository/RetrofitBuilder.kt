3c7a6865f08db010066b9c78149304ec7f5ad9b1
Bruno-Mac <Binladen@Bruno-Macs-MacBook-Pro.local>
2020-05-21 02:18:22 -0300

    organiza gradle e configura url em buildConfig

--- app/src/main/java/com/example/githubapitest/repository/RetrofitBuilder.kt
+++ app/src/main/java/com/example/githubapitest/repository/RetrofitBuilder.kt
@@ -1,21 +1,13 @@
 package com.example.githubapitest.repository
 
+import com.example.githubapitest.BuildConfig
 import com.example.githubapitest.helper.extensions.build
 import com.example.githubapitest.helper.extensions.provideInterface
 import com.example.githubapitest.repository.interfaces.GithubMethods
 import retrofit2.Retrofit
 
 class RetrofitBuilder {
-    private val retrofit: Retrofit
-
+    private val retrofit: Retrofit = Retrofit.Builder().build(BuildConfig.url)
     val service: GithubMethods
         get() = retrofit.provideInterface()
-
-    init {
-        retrofit = Retrofit.Builder().build(BASE_URL)
-    }
-
-    companion object {
-        const val BASE_URL = "https://api.github.com"
-    }
 }
